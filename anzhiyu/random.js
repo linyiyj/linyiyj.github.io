1b6deaa4b2708eaa1b2c095fb905420feb99dad0
杨见 <yangjian@yangjiandeMacBook-Pro.local>
2025-06-06 18:00:19 +0800

    Site updated: 2025-06-06 18:00:18

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/06/04/hello-world/","2025/06/04/test/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
--- js/custom.js
+++
@@ -1,7 +0,0 @@
-// 随机生成彩色粒子轨迹
-document.addEventListener('mousemove', e => {
-  const particle = document.createElement('div');
-  particle.style = `left:${e.pageX}px;top:${e.pageY}px;background:hsl(${Math.random()*360},70%,60%)`;
-  document.body.appendChild(particle);
-  setTimeout(() => particle.remove(), 1000);
-});
